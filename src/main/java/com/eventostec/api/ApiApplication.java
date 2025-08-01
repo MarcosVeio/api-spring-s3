4da8aacd8123861aef6be328d59f59ee69ca86ab
Marcos Martinelli <marcos.joaquim.martinelli@gmail.com>
2025-08-01 17:06:23 -0300

    fix: rename title doc swagger and remove props database and s3

--- src/main/java/com/eventostec/api/ApiApplication.java
+++ src/main/java/com/eventostec/api/ApiApplication.java
@@ -1,8 +1,17 @@
 package com.eventostec.api;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API - Java Spring - aws S3",
+				version = "1.0.0",
+				description = "Documentação da API com Swagger OpenAPI"
+		)
+)
 @SpringBootApplication
 public class ApiApplication {
 
